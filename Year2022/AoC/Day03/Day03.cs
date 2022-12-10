49b4b2d5e0275a2cbdd9d467c4fb74d525b12a08
Michal Vácha <michal.vacha@live.com>
2022-12-10 22:36:53 +0100

    Updated day 3, gitignore

--- Year2022/AoC/Day03/Day03.cs
+++ Year2022/AoC/Day03/Day03.cs
@@ -18,25 +18,5 @@ public class Day03 : IDay
         => File.ReadAllLines(file).Select(GetDuplicatedItem).Select(ScoreItem).Sum();
 
     public object SolveSecond(string file)
-        => File.ReadAllLines(file).SelectThree((l1, l2, l3) => ScoreItem(l1.Intersect(l2).Intersect(l3).First())).Sum();
-}
-
-public static class LinqExtensions
-
-{
-    public static IEnumerable<TResult> SelectThree<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TSource, TSource, TResult> selector)
-    {
-        using var iterator = source.GetEnumerator();
-
-        while (iterator.MoveNext())
-        {
-            var item1 = iterator.Current;
-            iterator.MoveNext();
-            var item2 = iterator.Current;
-            iterator.MoveNext();
-            var item3 = iterator.Current;
-
-            yield return selector(item1, item2, item3);
-        }
-    }
+        => File.ReadAllLines(file).Chunk(3).Select(group => ScoreItem(group[0].Intersect(group[1]).Intersect(group[2]).First())).Sum();
 }
