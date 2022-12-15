32d4291f57feecf39b67cfb809368074add5bf5d
Michal Vácha <michal.vacha@live.com>
2022-12-15 22:29:53 +0100

    Day 15 cleanup

--- Year2022/AoC/Day15/Day15.cs
+++ Year2022/AoC/Day15/Day15.cs
@@ -1,4 +1,4 @@
-using static Day15;
+using System.Diagnostics;
 
 public class Day15 : IDay
 {
@@ -24,49 +24,11 @@ public class Day15 : IDay
         public static int ManhattanDist(Coord a, Coord b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
     }
 
-    public record Signal(Coord Left, Coord Top, Coord Right, Coord Bottom)
+    public record Reading(Coord Sensor, Coord Beacon, int Distance)
     {
-        public static Signal FromSensor(Coord loc, int distance)
-            => new(loc + (distance * LEFT), loc + (distance * UP), loc + (distance * RIGHT), loc + (distance * DOWN));
+        public bool InRange(Coord pos) => Coord.ManhattanDist(Sensor, pos) <= Distance;
     }
-
-    public record Reading(Coord sensor, Coord beacon, int Distance)
-    {
-        public bool InRange(Coord pos) => Coord.ManhattanDist(sensor, pos) <= Distance;
-    }
-
-    public object SolveFirst(string inputFile)
-    {
-        var signals = new List<Signal>();
-        var readings = new List<Reading>();
-
-        foreach (var line in File.ReadAllLines(inputFile))
-        {
-            var parts = line.Split(',').Select(int.Parse).ToArray();
-            var sensor = new Coord(parts[0], parts[1]);
-            var beacon = new Coord(parts[2], parts[3]);
-            var reading = new Reading(sensor, beacon, Coord.ManhattanDist(sensor, beacon));
-            var signal = Signal.FromSensor(reading.sensor, reading.Distance);
-
-            readings.Add(reading);
-            signals.Add(signal);
-        }
-
-        var left = signals.Min(s => s.Left.X);
-        var right = signals.Max(s => s.Right.X);
-
-        int y = (inputFile == "input.txt") ? 2000000 : 10;
-        int count = 0;
-        for (int x = left; x <= right; x++)
-        {
-            var pos = new Coord(x, y);
-            if (readings.Any(r => pos != r.beacon && r.InRange(pos)))
-                count++;
-        }
-
-        return count;
-    }
-
+    
     public record Rect(Coord TopLeft, int Height, int Width)
     {
         public Coord TopRight = TopLeft + (Width - 1) * RIGHT;
@@ -86,7 +48,51 @@ public class Day15 : IDay
             yield return new(TopLeft + newH1 * DOWN, newH2, newW1); //bottom right
             yield return new(TopLeft + newH1 * DOWN + newW1 * RIGHT, newH2, newW2); //bottom right
         }
-        
+
+    }
+
+    public static List<Reading> ParseReading(IEnumerable<string> lines)
+    {
+        var readings = new List<Reading>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(',').Select(int.Parse).ToArray();
+            var sensor = new Coord(parts[0], parts[1]);
+            var beacon = new Coord(parts[2], parts[3]);
+            var reading = new Reading(sensor, beacon, Coord.ManhattanDist(sensor, beacon));
+
+            readings.Add(reading);
+        }
+
+        return readings;
+    }
+
+    public object SolveFirst(string inputFile)
+    {
+        var readings = ParseReading(File.ReadAllLines(inputFile));
+
+        var leftSensor = readings.MinBy(s => s.Sensor.X)!;
+        var rightSensor = readings.MaxBy(s => s.Sensor.X)!;
+
+        var left = leftSensor.Sensor.X - leftSensor.Distance;
+        var right = rightSensor.Sensor.X + rightSensor.Distance;
+
+        int y = (inputFile == "input.txt") ? 2000000 : 10;
+        int count = 0;
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int x = left; x <= right; x++)
+        {
+            var pos = new Coord(x, y);
+            if (readings.Any(r => pos != r.Beacon && r.InRange(pos)))
+                count++;
+        }
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"First part took: {stopwatch.Elapsed.Milliseconds}ms.");
+        return count;
     }
 
     public Rect? FindUncovered(IList<Reading> readings, Rect rect)
@@ -95,12 +101,13 @@ public class Day15 : IDay
         {
             if (readings.Any(s => r.Corners.All(s.InRange))) //all corners are covered by a single signal -> end this square
                 continue;
-            if (r.Height == 1 && r.Width == 1) //uncovered
+
+            if (r.Height == 1 && r.Width == 1) //uncovered (we know it's 1 by 1)
                 return r;
 
             var uncovered = FindUncovered(readings, r);
             if (uncovered != null)
-                return uncovered;
+                return uncovered; //not-null returned from recursion -> uncovered rect found
         }
 
         return null;
@@ -108,25 +115,19 @@ public class Day15 : IDay
 
     public object SolveSecond(string inputFile)
     {
-        var signals = new List<Signal>();
-        var readings = new List<Reading>();
-
-        foreach (var line in File.ReadAllLines(inputFile))
-        {
-            var parts = line.Split(',').Select(int.Parse).ToArray();
-            var sensor = new Coord(parts[0], parts[1]);
-            var beacon = new Coord(parts[2], parts[3]);
-            var reading = new Reading(sensor, beacon, Coord.ManhattanDist(sensor, beacon));
-            var signal = Signal.FromSensor(reading.sensor, reading.Distance);
-
-            readings.Add(reading);
-            signals.Add(signal);
-        }
+        var readings = ParseReading(File.ReadAllLines(inputFile));
 
         int limit = inputFile.EndsWith("input.txt") ? 4000000 : 20;
-        var rect = FindUncovered(readings, new(new(0, 0), limit, limit));
-        
-        
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        var rect = FindUncovered(readings, new(new(0, 0), limit, limit))!;
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Second part took: {stopwatch.Elapsed.Milliseconds}ms.");
+
         return rect.TopLeft.X * 4000000L + rect.TopLeft.Y;
     }
 }
